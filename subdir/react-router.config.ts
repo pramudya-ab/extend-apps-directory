189c4d94e5d2c9b116674d29a1d761f5ca660365
Abdullah Ammar <abdullah.ammar@accelbyte.net>
2025-11-19 13:57:53 +0700

    fix: copy index to 404

--- subdir/react-router.config.ts
+++ subdir/react-router.config.ts
@@ -4,6 +4,5 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
-  prerender: ['/'],
-  basename: '/extend-apps-explorer/'
+  prerender: ['/']
 } satisfies Config;
