3bdfd9d710da7b246cff9a68308b61304cbd62bf
Abdullah Ammar <abdullah.ammar@accelbyte.net>
2025-11-19 13:26:04 +0700

    fix: remove vite base path and let react router handle it

--- subdir/vite.config.ts
+++ subdir/vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  base: '/extend-apps-explorer/',
   plugins: [reactRouter(), tsconfigPaths()]
 })
